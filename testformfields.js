cbd6fc03ee81a8993ad136c758a6b4f9cef1ea01
Cesar Hernandez <cesar.hernandez1094@gmail.com>
2021-06-11 12:18:43 -0400

    Update JS file

--- testformfields.js
+++ testformfields.js
@@ -1,15 +1,45 @@
 (function () {
   window.addEventListener('load', function () {
-    var sourceControlId = 97270574,
-      targetControlId = 97270677,
-      sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId),
-      targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
+    // Hobbies Related
+    const hobbiesControlID = 97270574;
+    const hobbisHiddenControlID = 97270574;
+    const hobbiesCheckBoxField = loader.getEngine().getDocument().getElementById(hobbiesControlID);
+    const hobbiesHiddenTextField = loader.getEngine().getDocument().getElementById(hobbisHiddenControlID);
 
-    sourceControlInstance.on('value-change', function () {
-      const values = sourceControlInstance.getValue();
-      targetControlInstance.setValue({ value: values.values.join() });
+    hobbiesCheckBoxField.on('value-change', function () {
+      const checkBoxes = hobbiesCheckBoxField.getValue();
+
+      hobbiesHiddenTextField.setValue({ value: checkBoxes.values.join() });
     });
 
-    targetControlInstance.setValue({ value: sourceControlInstance.getValue().values.join() });
+    hobbiesHiddenTextField.setValue({ value: hobbiesCheckBoxField.getValue().values.join() });
+
+    // Animals Related
+    const animalControlID = 97387782;
+    const animalHiddenControlID = 97387801;
+    const animalsCheckBoxField = loader.getEngine().getDocument().getElementById(animalControlID);
+    const animalsHiddenTextField = loader.getEngine().getDocument().getElementById(animalHiddenControlID);
+
+    animalsCheckBoxField.on('value-change', function () {
+      const checkBoxes = animalsCheckBoxField.getValue();
+
+      animalsHiddenTextField.setValue({ value: checkBoxes.values.join() });
+    });
+
+    animalsHiddenTextField.setValue({ value: animalsCheckBoxField.getValue().values.join() });
+
+    // Colors Related
+    const colorsControlID = 97487815;
+    const colorsHiddenControlID = 97387818;
+    const colorsCheckBoxField = loader.getEngine().getDocument().getElementById(colorsControlID);
+    const colorsHiddenTextField = loader.getEngine().getDocument().getElementById(colorsHiddenControlID);
+
+    colorsCheckBoxField.on('value-change', function () {
+      const checkBoxes = animalsCheckBoxField.getValue();
+
+      colorsHiddenTextField.setValue({ value: checkBoxes.values.join() });
+    });
+
+    colorsHiddenTextField.setValue({ value: colorsCheckBoxField.getValue().values.join() });
   });
 })();
