430b29f6a1be97fb3c9d0edeaccdc2e752855f58
Cesar Hernandez <cesar.hernandez1094@gmail.com>
2022-05-10 11:39:36 -0400

    feat(123Forms): update logic for form calculations

--- vrl-form.js
+++ vrl-form.js
@@ -1,29 +1,67 @@
 (function () {
   init();
 
+  let winNumberField;
+  let turnoutOneField;
+  let turnoutTwoField;
+  let turnoutThreeField;
+  let peopleRegisteredField;
+
   /**
    * @link https://www.123formbuilder.com/blog/pass-field-input-value-to-another-field/#How_to_Pass_Value_From_One_Form_To_Another_Form
    */
-  function handleVotesNeededField() {
-    const votesNeededFieldID = 101408730;
+  //function handleVotesNeededField() {
+  //  const votesNeededFieldID = 101408730;
+  //
+  //  const votesNeededField = loader.getEngine().getDocument().getElementById( votesNeededFieldID );
+  //
+  //  votesNeededField.on( 'value-change', function () {
+  //    const fieldValue = votesNeededField.getValue().value;
+  //
+  //    // Ensure field value we get is a number
+  //    if ( isNaN( fieldValue ) ) {
+  //      return;
+  //    }
+  //
+  //    votesNeededField.setValue( { value: Math.ceil( fieldValue ) } );
+  //  } )
+  //}
 
-    const votesNeededField = loader.getEngine().getDocument().getElementById( votesNeededFieldID );
+  function bindEvents() {
+    turnoutOneField.on( 'value-change', handleUpdatingWinNumberField );
+    turnoutTwoField.on( 'value-change', handleUpdatingWinNumberField );
+    turnoutThreeField.on( 'value-change', handleUpdatingWinNumberField );
+    peopleRegisteredField.on( 'value-change', handleUpdatingWinNumberField );
+  }
 
-    votesNeededField.on( 'value-change', function () {
-      const fieldValue = votesNeededField.getValue().value;
+  function handleUpdatingWinNumberField() {
+    if (
+      isNaN( peopleRegisteredField.getValue().value )
+      || isNaN( turnoutOneField.getValue().value )
+      || isNaN( turnoutTwoField.getValue().value )
+      || isNaN( turnoutThreeField.getValue().value ) ) {
+      return;
+    }
 
-      // Ensure field value we get is a number
-      if ( isNaN( fieldValue ) ) {
-        return;
-      }
+    const calculation =
+            1 +
+            (.5
+              * (peopleRegisteredField.getValue().value * ((turnoutOneField.getValue().value + turnoutTwoField.getValue().value + turnoutThreeField.getValue().value) / 300)));
 
-      votesNeededField.setValue( { value: Math.ceil( fieldValue ) } );
-    } )
+    winNumberField.setValue( { value: Math.ceil( calculation ) } );
   }
 
   function init() {
     window.addEventListener( 'load', function () {
-      handleVotesNeededField();
+      winNumberField = loader.getEngine().getDocument().getElementById( 101740683 );
+
+      turnoutOneField   = loader.getEngine().getDocument().getElementById( 101408680 );
+      turnoutTwoField   = loader.getEngine().getDocument().getElementById( 101408689 );
+      turnoutThreeField = loader.getEngine().getDocument().getElementById( 101408684 );
+
+      peopleRegisteredField = loader.getEngine().getDocument().getElementById( 101408726 );
+
+      bindEvents();
     } );
   }
 })()
